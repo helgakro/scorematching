63e8154b9a7c62239f1ca299f58ecd5b092d9ce7
helgakro <helgakro@gmail.com>
2024-04-15 17:27:27 +0200

    set plot theme

---
+++ inst/scripts/set_plot_theme.R
@@ -0,0 +1,5 @@
+######################################
+# Set plot theme
+######################################
+theme_set(theme_bw())
+theme_update(text = element_text(family = "serif", size=12),plot.title = element_text(hjust = 0.5))
