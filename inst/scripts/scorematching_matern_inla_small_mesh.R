3624793713407d4bf3af18448b5069434e4b4b28
Helga <helgakro@gmail.com>
2024-04-16 15:42:04 +0200

    direct approach implemented for small mesh and benchmarked

--- inst/scripts/scorematching_matern_inla_small_mesh.R
+++ inst/scripts/scorematching_matern_inla_small_mesh.R
@@ -34,7 +34,7 @@ m <- m+rnorm(n=n*n_sample,mean = 0,sd = sigma_val) #added noise
 
 
 
-my_obj_func_new <- function(par){
+my_obj_func_new_x <- function(par){
   print(par)
   theta <- par
   mu <- rep(0,n)
@@ -46,11 +46,47 @@ my_obj_func_new <- function(par){
   return(loo_log_score_eps(m,mux,Qx,sigma_val,A))
 }
 
+my_obj_func_new_y <- function(par){
+  print(par)
+  theta <- par
+  mu <- rep(0,n)
+  #Qxy <- inla.spde.precision(spde, theta=theta) +I*sigma_val^2
+  Qx <- inla.spde.precision(spde, theta=theta)
+  Qeps <- Qeps <- I/sigma_val^2
+  Qtheta <- Qx-Qx%*%solve(Qx+Qeps)%*%Qx
+  mux <- mu
+  #if(sigma_val>0) muxy<- muxy+solve(Qxy,(I/sigma_val^2)%*%(t(m)-I%*%mu))
+  score <- loo_score_vectorised(m,mux,Qtheta)
+  print(score)
+  return(score)
+}
+
+my_log_obj_func_y <- function(par){
+  print(par)
+  theta <- par
+  mu <- rep(0,n)
+  #Qxy <- inla.spde.precision(spde, theta=theta) +I*sigma_val^2
+  Qx <- inla.spde.precision(spde, theta=theta)
+  Qeps <- I/sigma_val^2
+  Qtheta <- Qx-Qx%*%solve(Qx+Qeps)%*%Qx
+  mux <- mu
+  #if(sigma_val>0) muxy<- muxy+solve(Qxy,(I/sigma_val^2)%*%(t(m)-I%*%mu))
+  score <- -log_dmvn(m,mux,Qtheta)
+  print(score)
+  return(score)
+}
+
 kappa0<-1
 tau0<-0.1
 
 
-o1<-optim(par=c(kappa0,tau0),my_obj_func_new,control=list(maxit=50000))
+microbenchmark::microbenchmark(o1<-optim(par=c(kappa0,tau0),my_obj_func_new_x,control=list(maxit=50000)),
+o2<-optim(par=c(kappa0,tau0),my_obj_func_new_y,control=list(maxit=50000)),times=1)
+
+o2<-optim(par=c(kappa0,tau0),my_log_obj_func_y,control=list(maxit=50000))
+
+microbenchmark::microbenchmark(my_obj_func_new_x(c(kappa0,tau0)),my_obj_func_new_y(c(kappa0,tau0)),times=5)
+
 
 loo_score_vectorised_eps(m,mu,Q,sigma_val,A)
 loo_log_score_eps(m,mu,Q,sigma_val,A)
@@ -59,6 +95,8 @@ o1$value
 o1$par
 spde$param.inla$theta.initial
 my_obj_func_new(spde$param.inla$theta.initial)
+
+my_log_obj_func_y(spde$param.inla$theta.initial)
 
 ##################### Test S-M
 
@@ -81,6 +119,12 @@ n_res=n_rep
 score_par <- sapply(res_no_outliers_nresp_small$o_sroot[1:n_res], function(o) o$par)
 log_score_par <- sapply(res_no_outliers_nresp_small$o_slog[1:n_res], function(o) o$par)
 print(sum(sapply(res_no_outliers_nresp_small$o_sroot[1:n_res], function(o) o$convergence)))
+
+par_df <- data.frame(method=rep(c("Sroot","Slog"),each=n_res), par = t(cbind(score_par,log_score_par)), run.time=c(res_no_outliers_nresp_small$times_sroot[1:n_res],res_no_outliers_nresp_small$times_slog[1:n_res]),i=rep(c(1:n_res),2))
+par_df_mean <- par_df %>%
+  group_by(method) %>%
+  summarize(meanmu = mean(mu),meanrho = mean(rho))
+
 par_df <- data.frame(method=rep(c("Sroot","Slog"),each=n_res), mu = c(score_par[1,],log_score_par[1,]), rho = c(score_par[2,],log_score_par[2,]), run.time=c(res_no_outliers_nresp_small$times_sroot[1:n_res],res_no_outliers_nresp_small$times_slog[1:n_res]),i=rep(c(1:n_res),2))
 par_df_mean <- par_df %>%
   group_by(method) %>%
@@ -100,3 +144,10 @@ p.time.hist <- ggplot(par_df,aes(x=run.time,color=method,fill=method))+geom_hist
 
 
 res_no_outliers_nresp_small_2 <- repeated_inference_norm_resp(spde,mesh_sim$n,5,Q,sigma_val=0.01)
+p.res_no_outliers_nresp_small<- plot_results(res_no_outliers_nresp_small)
+p.res_no_outliers_nresp_small$p.scatter
+p.res_no_outliers_nresp_small$p.hist.mu
+p.res_no_outliers_nresp_small$p.hist.rho
+p.res_no_outliers_nresp_small$p.time
+p.res_no_outliers_nresp_small$p.time.hist
+
